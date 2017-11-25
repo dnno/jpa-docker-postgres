ca7aca865fe94e5c0f43523e0959b95bfe0a6658
Reinhard Prechtl <reinhard.prechtl@codecentric.de>
2017-11-25 10:02:04 +0100

    Initial

---
+++ src/main/java/de/rpr/jpadockerpostgres/JpaDockerPostgresApplication.java
@@ -0,0 +1,12 @@
+package de.rpr.jpadockerpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaDockerPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaDockerPostgresApplication.class, args);
+	}
+}
